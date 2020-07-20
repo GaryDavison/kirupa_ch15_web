31f928e47db7c9c7f98a5bebc7bf584ef0b00cdd
Gary Davison <gary.davison@jhu.edu>
2020-07-20 02:38:52 -0400

    Changed index.js. Added HelloWorld.js. Changed index.css. Changed index.js. Added HelloWorld.css. Changed Helloworld.js

---
+++ src/HelloWorld.js
@@ -0,0 +1,18 @@
+// HelloWorld.js
+// Source: https://www.kirupa.com/react/setting_up_react_environment.htm
+
+import React, { Component } from 'react';
+import './HelloWorld.css';
+
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="helloContainer">
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
